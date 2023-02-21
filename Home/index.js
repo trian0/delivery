b92b7aac6c0440939a5d1081c0f48675d3ee1cb4
Thales Rian <thalesrian0@gmail.com>
2023-02-21 08:38:43 -0300

    feat(Home): cria página inicial

---
+++ Home/index.js
@@ -0,0 +1,11 @@
+import { Container, Title} from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Title>Desafio Delivery</Title>
+    </Container>
+  );
+}
+
+export default Home;
