72b8ee4c058811b4d2420f98ca02183deadcb7ce
Thales Rian <thalesrian0@gmail.com>
2023-02-21 14:57:15 -0300

    feat(coffees.js): criação dearray de cafés

---
+++ assets/coffees.js
@@ -0,0 +1,38 @@
+export const coffees = [
+    {
+      id: 1,
+      tags: ['TRADICIONAL'],
+      name: 'Expresso Tradicional',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      photo: 'tradicional.png',
+      price: 9.9,
+      quantity: 0,
+    },
+    {
+      id: 2,
+      tags: ['TRADICIONAL'],
+      name: 'Expresso Americano',
+      description: 'Expresso diluído, menos intenso que o tradicional',
+      photo: 'americano.png',
+      price: 9.9,
+      quantity: 0,
+    },
+    {
+      id: 3,
+      tags: ['TRADICIONAL'],
+      name: 'Expresso Cremoso',
+      description: 'Café expresso tradicional com espuma cremosa',
+      photo: 'cremoso.png',
+      price: 9.9,
+      quantity: 0,
+    },
+    {
+      id: 4,
+      tags: ['TRADICIONAL', 'GELADO'],
+      name: 'Expresso Gelado',
+      description: 'Bebida preparada com café expresso e cubos de gelo',
+      photo: 'gelado.png',
+      price: 9.9,
+      quantity: 0,
+    },
+  ]
